386ed3b0343a4425b9a9d4c7fc472d7cacd7aa4b
Simion <simion.okola@student.moringaschool.com>
2022-10-29 23:25:49 +0300

    Initialize Magazine

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,8 +1,29 @@
-class Magazine
+attr_accessor :name, :category
 
-  
+    @@all= []
 
+  def initialize(name, category)
+    @name = name
+    @category = category
+    @@all << self
+  end
+
+def self.all
+  @@all
 end
 
 
+daily= Magazine.new("The standard", "Daily News")
+dail= Magazine.new("Taifa Leo", "Taarifa")
+dai= Magazine.new("Nation", "Daily News")
 
+puts daily.name
+puts daily.category
+
+puts dail.name
+puts dail.category
+
+puts dai.name
+puts dai.category
+
+puts Magazine.all
