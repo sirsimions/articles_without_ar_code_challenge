cdb0eac2c5934f1e889206f5f9c350e4ad13670b
Simion <simion.okola@student.moringaschool.com>
2022-10-29 23:42:38 +0300

    Intitialize Articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,30 @@
 class Article
+    attr_reader :author, :magazine, :title
+    
+    @@all = []
 
+    def initialize(author, magazine, title)
+        @author = author
+        @magazine = magazine
+        @title = title
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 end
+
+
+art1= Article.new("Kazungu Tajiri", "Maisha Leo", "Tembea Kenya")
+art2= Article.new("Katana Mlenga", "Life style", "How to stop smoking")
+
+puts art1.author
+puts art1.magazine
+puts art1.title
+
+puts art2.author
+puts art2.magazine
+puts art2.title
+
