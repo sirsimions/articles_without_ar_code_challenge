d13fd1c8d9a71d46f4deb0a10299c09e89c00276
Simion <simion.okola@student.moringaschool.com>
2022-10-31 16:48:21 +0300

    Fixing errors

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -15,12 +15,17 @@ def self.all
   @@all
 end
 
-def contributors
-  Article.all.filter do |magazine|
-    magazine.magazine == self
-  end.collect do |magz|
-    magz.author
-  end
+# def contributors
+#   Article.all.filter do |magazine|
+#     magazine.magazine == self
+#   end.collect do |magz|
+#     magz.author
+#   end
+# end
+
+def contributors 
+  articles = Article.all.filter {|article| self == article.magazine}
+  articles.map{|article| article.author}
 end
 
 def self.find_by_name(name)
@@ -37,14 +42,21 @@ def article_titles
   end
 end
 
+# def contributing_authors
+#   Article.all.select do |magazine|
+#     magazine.magazine == self
+#   end.collect do |author|
+#     author.author
+#   end.uniq.collect do |article|
+#     article.articles.count > 2
+#   end
+# end
+
 def contributing_authors
-  Article.all.select do |magazine|
-    magazine.magazine == self
-  end.collect do |author|
-    author.author
-  end.uniq.collect do |article|
-    article.articles.count > 2
-  end
+  articles =Article.all.filter {|article| self == article.magazine}
+  all_authors = articles.map{|article| article.author}
+  uniq_authors = all_authors.uniq 
+  uniq_authors.filter{|author| all_authors.count(author)>2}
 end
 
 end
